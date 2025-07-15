dda0786c984e1441942dd697189d53eb8e341374
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-07-14 21:42:07 -0300

    OPC - Add metodos

---
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -0,0 +1,36 @@
+public class PersonagemGame {
+
+    private String nome;
+    private int saudeAtual;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+    }
+
+    public void tomarDano(int quantidadeDeDano){
+      saudeAtual = getSaudeAtual() - quantidadeDeDano;
+      if(saudeAtual < 0){
+        saudeAtual = 0;
+      }
+    }
+
+    public void receberCura(int quantidadeDeCura){
+        saudeAtual = getSaudeAtual() + quantidadeDeDano;
+        if(saudeAtual > 100){
+            saudeAtual = 100;
+        }
+    }
+
+}
