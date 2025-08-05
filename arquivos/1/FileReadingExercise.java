5968902f9808a96b8209735f7164d7a4b0b2ffb6
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-08-05 17:50:36 -0300

    arquivos - FileReadingExercise

---
+++ arquivos/1/FileReadingExercise.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class FileReadingExercise {
+    public static void main(String[] args) {
+        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+
+        System.out.println("Conteúdo do arquivo '"+fileName+"'\n");
+
+        // efetue a leitura do arquivo
+
+        try {
+            BufferedReader leitor = new BufferedReader(new FileReader(fileName));
+
+            String linha = leitor.readLine();
+
+            while(linha != null) {
+                System.out.println(linha);
+                linha = leitor.readLine();
+            }
+            leitor.close();
+            System.out.println("\nLeitura do arquivo concluída.");
+        } catch (IOException  e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
