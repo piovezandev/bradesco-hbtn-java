8815ac9feab8991cb90a1b2577b04e0761948db1
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-08-05 18:50:02 -0300

    arquivos - CsvFileReader

--- arquivos/2/CsvFileReader.java
+++ arquivos/2/CsvFileReader.java
@@ -13,7 +13,7 @@ public class CsvFileReader {
 
             while (linha != null) {
 				String[] csv = linha.split(",");
-				System.out.print("teste: " +csv[0]);
+				System.out.print("Funcionário: " +csv[0]);
 				System.out.print("\nIdade: " +csv[1]);
 				System.out.print("\nDepartamento: " +csv[2]);
 				System.out.print("\nSalarial: " +csv[3]);
