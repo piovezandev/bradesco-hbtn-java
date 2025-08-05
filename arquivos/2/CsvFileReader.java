fd18fbec851e042dd1e2897fd1981311ff6ca3a0
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-08-05 18:43:00 -0300

    arquivos - CsvFileReader

---
+++ arquivos/2/CsvFileReader.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CsvFileReader {
+
+	public static void main(String[] args) {
+		String fileName = "funcionarios.csv";
+		try {
+			BufferedReader leitor = new BufferedReader(new FileReader(fileName));
+
+			String linha = leitor.readLine();
+
+            while (linha != null) {
+				String[] csv = linha.split(",");
+				System.out.print("Funcion�rio:" +csv[0]);
+				System.out.print("\nIdade:" +csv[1]);
+				System.out.print("\nDepartamento:" +csv[2]);
+				System.out.print("\nSalarial:" +csv[3]);
+				System.out.println("\n------------------------");
+				linha = leitor.readLine();
+			}
+
+            leitor.close();
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+}
