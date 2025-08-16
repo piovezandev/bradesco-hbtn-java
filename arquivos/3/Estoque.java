68d63ac74e437cdba6c9d2717b0c45d93f4e9f0d
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-08-16 17:24:06 -0300

    arquivos - estoque

--- arquivos/3/Estoque.java
+++ arquivos/3/Estoque.java
@@ -125,7 +125,7 @@ public class Estoque {
 
 			while (linha != null) {
 				String[] csv = linha.split(",");
-				if (csv.length >= 4) {
+				if (csv[0] != "") {
 					produto = new Produto(Integer.parseInt(csv[0]), csv[1], Integer.parseInt(csv[2]),
 							Double.parseDouble(csv[3]));
 				}
