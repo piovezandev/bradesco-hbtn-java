f47c2ba5ab7d97f668a55490fc588aedadd36bab
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-08-16 17:24:47 -0300

    arquivos - estoque

--- arquivos/3/Estoque.java
+++ arquivos/3/Estoque.java
@@ -128,9 +128,9 @@ public class Estoque {
 				if (csv[0] != "") {
 					produto = new Produto(Integer.parseInt(csv[0]), csv[1], Integer.parseInt(csv[2]),
 							Double.parseDouble(csv[3]));
+					produtos.add(produto);
 				}
 				linha = bufferedReader.readLine();
-				produtos.add(produto);
 			}
 
 			bufferedReader.close();
