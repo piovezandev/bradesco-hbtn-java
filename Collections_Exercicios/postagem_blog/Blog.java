129fd53202f23b67c912c09e73d2a856e68edf2a
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-09-12 16:13:01 -0300

    Collections_Exercicios - postagem_blog

--- Collections_Exercicios/postagem_blog/Blog.java
+++ Collections_Exercicios/postagem_blog/Blog.java
@@ -8,9 +8,9 @@ import java.util.Set;
 public class Blog {
 
 	private List<Post> postagens;
-	
+
 	public Blog() {
-		postagens = new ArrayList<>();
+		postagens = new ArrayList<Post>();
 	}
 
 	public void adicionarPostagem(Post post) {
@@ -24,7 +24,7 @@ public class Blog {
 	}
 
 	public Set<Autor> obterTodosAutores() {
-		Set<Autor> autores = new HashSet();
+		Set<Autor> autores = new HashSet<Autor>();
 		for (Post post : postagens) {
 			autores.add(post.getAutor());
 		}
@@ -32,29 +32,29 @@ public class Blog {
 	}
 
 	public Map<Categorias, Integer> obterContagemPorCategoria() {
-	     Map<Categorias, Integer> map = new HashMap<Categorias, Integer>();
+		Map<Categorias, Integer> map = new HashMap<Categorias, Integer>();
 
-	     for (Post post : postagens) {
-	    	 Categorias categoria = post.getCategoria();
-	    	 map.put(categoria, map.getOrDefault(categoria, 0)+1);
-		}	   
+		for (Post post : postagens) {
+			Categorias categoria = post.getCategoria();
+			map.put(categoria, map.getOrDefault(categoria, 0)+1);
+		}
 		return map;
 	}
 
 	public Set<Post> obterPostsPorCategoria(Categorias categorias) {
 		Set<Post> posts = new HashSet<Post>();
-		
+
 		for (Post post : postagens) {
-			 if(post.getCategoria().equals(categorias)) {
-		    	 posts.add(post); 
-			 }
-		}	   
+			if(post.getCategoria().equals(categorias)) {
+				posts.add(post);
+			}
+		}
 		return posts;
 	}
 
 	public Set<Post> obterPostsPorAutor(Autor autor) {
 		Set<Post> posts = new HashSet<Post>();
-		
+
 		for (Post post : postagens) {
 			if(post.getAutor().getNome().equalsIgnoreCase(autor.getNome())) {
 				posts.add(post);
@@ -64,25 +64,24 @@ public class Blog {
 	}
 
 	public Map<Categorias, Set<Post>> obterTodosPostsPorCategorias() {
-        Map<Categorias, Set<Post>> categorias = new HashMap<>();
+		Map<Categorias, Set<Post>> categorias = new HashMap<Categorias, Set<Post>>();
+		for (Post post : postagens) {
+			Categorias categoria = post.getCategoria();
+			categorias.computeIfAbsent(categoria, k -> new HashSet<>()).add(post);
+		}
 
-        for (Post post : postagens) {
-            Categorias categoria = post.getCategoria();
-            categorias.computeIfAbsent(categoria, k -> new HashSet<>()).add(post);
-        }
-
-        return categorias;
+		return categorias;
 	}
 
 	public Map<Autor, Set<Post>> obterTodosPostsPorAutor() {
-		Map<Autor, Set<Post>> autores = new HashMap<>();
+		Map<Autor, Set<Post>> autores = new HashMap<Autor, Set<Post>>();
 
-        for (Post post : postagens) {
-            Autor autor = post.getAutor();
-            autores.computeIfAbsent(autor, k -> new HashSet<>()).add(post);
-        }
+		for (Post post : postagens) {
+			Autor autor = post.getAutor();
+			autores.computeIfAbsent(autor, k -> new HashSet<>()).add(post);
+		}
 
-        return autores;
+		return autores;
 	}
 
 }
