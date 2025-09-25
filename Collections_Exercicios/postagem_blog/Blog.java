62c82922ccc87b43b4bff768d7bd5bb20f7508fb
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-09-25 15:45:09 -0300

    Collections_Exercicios - postagem_blog

--- Collections_Exercicios/postagem_blog/Blog.java
+++ Collections_Exercicios/postagem_blog/Blog.java
@@ -1,32 +1,25 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Blog {
 
 	private List<Post> postagens;
-	
+
 	public Blog() {
-		postagens = new ArrayList<Post>();
+		postagens = new ArrayList<>();
 	}
 
 	public void adicionarPostagem(Post post) {
 		for (Post postagem : postagens) {
 			if (postagem.getAutor().getNome().equalsIgnoreCase(post.getAutor().getNome())
 					&& postagem.getTitulo().equalsIgnoreCase(post.getTitulo())) {
-				throw new IllegalArgumentException("Postagem jah existente");
+				throw new IllegalArgumentException("Postagem já existente");
 			}
 		}
 		postagens.add(post);
 	}
 
 	public Set<Autor> obterTodosAutores() {
-		Set<Autor> autores = new TreeSet<Autor>(Comparator.comparing(Autor::getNome));
+		Set<Autor> autores = new TreeSet<>(Comparator.comparing(Autor::getNome));
 		for (Post post : postagens) {
 			autores.add(post.getAutor());
 		}
@@ -34,29 +27,28 @@ public class Blog {
 	}
 
 	public Map<Categorias, Integer> obterContagemPorCategoria() {
-	     Map<Categorias, Integer> map = new TreeMap<Categorias, Integer>(Comparator.comparing(Categorias::getValores).reversed());
-	     
-	     for (Post post : postagens) {
-	    	 Categorias categoria = post.getCategoria();
-	    	 map.put(categoria, map.getOrDefault(categoria, 0)+1);
-		}	   
+		Map<Categorias, Integer> map = new TreeMap<>(); // TreeMap ordena pelas chaves naturalmente
+		for (Post post : postagens) {
+			Categorias categoria = post.getCategoria();
+			map.put(categoria, map.getOrDefault(categoria, 0) + 1);
+		}
 		return map;
 	}
 
-	public Set<Post> obterPostsPorCategoria(Categorias categorias) {
-	    Set<Post> posts = new TreeSet<Post>(Comparator.comparing(Post::getTitulo));
+	public Set<Post> obterPostsPorCategoria(Categorias categoriaDesejada) {
+		Set<Post> posts = new TreeSet<>(Comparator.comparing(Post::getTitulo));
 		for (Post post : postagens) {
-			 if(post.getCategoria().equals(categorias)) {
-		    	 posts.add(post); 
-			 }
-		}	   
+			if (post.getCategoria().equals(categoriaDesejada)) {
+				posts.add(post);
+			}
+		}
 		return posts;
 	}
 
 	public Set<Post> obterPostsPorAutor(Autor autor) {
-		Set<Post> posts = new TreeSet<Post>(Comparator.comparing(Post::getTitulo));
+		Set<Post> posts = new TreeSet<>(Comparator.comparing(Post::getTitulo));
 		for (Post post : postagens) {
-			if(post.getAutor().getNome().equalsIgnoreCase(autor.getNome())) {
+			if (post.getAutor().getNome().equalsIgnoreCase(autor.getNome())) {
 				posts.add(post);
 			}
 		}
@@ -64,22 +56,22 @@ public class Blog {
 	}
 
 	public Map<Categorias, Set<Post>> obterTodosPostsPorCategorias() {
-        Map<Categorias, Set<Post>> categorias = new HashMap<Categorias, Set<Post>>();
-        for (Post post : postagens) {
-            Categorias categoria = post.getCategoria();
-            categorias.computeIfAbsent(categoria, k -> new TreeSet<>(Comparator.comparing(Post::getTitulo))).add(post);
-        }
-
-        return categorias;
+		Map<Categorias, Set<Post>> categorias = new HashMap<>();
+		for (Post post : postagens) {
+			categorias.computeIfAbsent(post.getCategoria(), k ->
+					new TreeSet<>(Comparator.comparing(Post::getTitulo))
+			).add(post);
+		}
+		return categorias;
 	}
 
 	public Map<Autor, Set<Post>> obterTodosPostsPorAutor() {
-		Map<Autor, Set<Post>> autores = new TreeMap<Autor, Set<Post>>(Comparator.comparing(Autor::getNome));
-        for (Post post : postagens) {
-            Autor autor = post.getAutor();
-            autores.computeIfAbsent(autor, k -> new TreeSet<>(Comparator.comparing(Post::getAutor))).add(post);
-        }
-        return autores;
+		Map<Autor, Set<Post>> autores = new TreeMap<>(Comparator.comparing(Autor::getNome));
+		for (Post post : postagens) {
+			autores.computeIfAbsent(post.getAutor(), k ->
+					new TreeSet<>(Comparator.comparing(Post::getTitulo))
+			).add(post);
+		}
+		return autores;
 	}
-
 }
