012975041c56720820d5e4f62867e31b2b006957
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-09-30 16:24:50 -0300

    Streams_Collectors_Optional - maior_preco

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+		return pedido
+		.getProdutos()
+		.stream()
+		.filter(p -> p.getCategoria().compareTo(CategoriaProduto.LIVRO) == 0)
+		.collect(Collectors.toList());
+	}
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+		Produto produto = produtos
+				.stream()
+				.sorted(Comparator.comparing(Produto::getPreco).reversed()).findFirst()
+				.orElse(null);
+		return produto;
+	}
+
+}
