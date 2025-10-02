7fac8e996305a005f9aa9dd6805efc83db206047
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-02 16:50:06 -0300

    0x19 - acima_40

---
+++ 0x19/acima_40/ConsultaPessoas.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+	public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> todasPessoas) {
+		return todasPessoas
+				.stream()
+				.collect(Collectors.groupingBy(Pessoa::getCargo, 
+						Collectors.filtering(p -> p.getIdade() >= 40,  Collectors.toList())));
+	}
+
+}
