2c6c9274c8c0864cab3bad3bd98a6c0fc9a7545c
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-02 17:43:04 -0300

    Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,13 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+	public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> todasPessoas) {
+		return todasPessoas
+		.stream()
+		.collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+	}
+
+}
