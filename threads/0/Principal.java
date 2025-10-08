7a2a9317562a8143a765d87cf152973478940c46
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-08 16:15:22 -0300

    threads - corrige pois deve conter start ao inves de run

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -1,14 +1,17 @@
 public class Principal {
 
-    public static void main(String[] args) {
-    	Contador contador = new Contador();
-    	
-    	ThreadContador threadContador1 = new ThreadContador(contador);
-    	ThreadContador threadContador2 = new ThreadContador(contador);
-    	
-    	threadContador1.run();
-    	threadContador2.run();
- 
-    	System.out.println("Valor final do contador: " + contador.getContagem());
-    }
+	public static void main(String[] args) throws InterruptedException {
+		Contador contador = new Contador();
+
+		ThreadContador threadContador1 = new ThreadContador(contador);
+		ThreadContador threadContador2 = new ThreadContador(contador);
+
+		threadContador1.start();
+		threadContador2.start();
+
+		threadContador1.join();
+		threadContador2.join();
+
+		System.out.println("Valor final do contador: " + contador.getContagem());
+	}
 }
