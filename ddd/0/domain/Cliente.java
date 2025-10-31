9e294dcf15081b2115a221b8da7bfca58c164177
Alan Piovezan <alan.log.piovezan@gmail.com>
2025-10-31 00:35:05 -0300

    bradesco-hbtn-java - ddd/0

---
+++ ddd/0/domain/Cliente.java
@@ -0,0 +1,29 @@
+package domain;
+
+public class Cliente {
+
+	private String id;
+	private String nome;
+
+	public Cliente(String id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
